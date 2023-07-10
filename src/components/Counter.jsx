af73e4e544f2b423c119599728a65082837dfa19
Adrian Nastał <adriannastal9@gmail.com>
2023-07-10 17:55:08 +0200

    2

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,31 @@
 import { Text } from "./Text";
 import { IconAdd } from "./icons/IconAdd";
 import { IconMinus } from "./icons/IconMinus";
+import { useState } from "react";
+
+import styles from "./Counter.module.css";
 
 export const Counter = (props) => {
+  const [value, setValue] = useState(0);
+
+  const handleAdd = () => {
+    setValue(value + 1);
+  };
+
+  const handleMinus = () => {
+    if (value <= 0) return;
+    setValue(value - 1);
+  };
+
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Text>
-        {props.count} {props.text}
+        {value} {props.text}
       </Text>
-      <IconMinus />
-      <IconAdd />
+      <nav>
+        <IconMinus onClick={handleMinus} />
+        <IconAdd onClick={handleAdd} />
+      </nav>
     </div>
   );
 };
